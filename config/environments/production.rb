11c4593bcf0b5b50dfa781ddecd465644803a0c6
rahulsaw13 <47470066+rahulsaw13@users.noreply.github.com>
2025-07-13 21:25:45 +0530

    q

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,9 +105,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "http://sample.sherafox.com"
-  # ]
+  config.hosts = [
+    "https://render-app-7ch2.onrender.com"
+  ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
